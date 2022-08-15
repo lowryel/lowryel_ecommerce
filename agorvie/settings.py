43ebfe368fae9cb0cf32fbc291a4e31e1baecb34
lowryel <eugeneagbaglo@gmail.com>
2022-08-15 12:44:02 +0000

    a

--- agorvie/settings.py
+++ agorvie/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mf&h1hn3=q(btlj9ndg06e0_#3j+(1k%^&*&f($8kbe)9mw(ss'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
