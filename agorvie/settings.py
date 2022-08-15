a6ae51ad18aa17fe090b14135d384227a292133c
lowryel <eugeneagbaglo@gmail.com>
2022-08-15 13:28:41 +0000

    a

--- agorvie/settings.py
+++ agorvie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mf&h1hn3=q(btlj9ndg06e0_#3j+(1k%^&*&f($8kbe)9mw(ss'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1:8000", "el-commerce.herokuapp.com"]
 
 
 # Application definition
