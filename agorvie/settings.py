863cce03ada1c6d73deb210ef9c886bc2da51fc2
lowryel <eugeneagbaglo@gmail.com>
2022-08-18 22:29:10 +0000

    updates for eb

--- agorvie/settings.py
+++ agorvie/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'mf&h1hn3=q(btlj9ndg06e0_#3j+(1k%^&*&f($8kbe)9mw(ss'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "el-commerce.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "el-commerce.herokuapp.com",
+                 "lowrycom.elasticbeanstalk.com"]
 # ALLOWED_HOSTS = []
 
 # Application definition
